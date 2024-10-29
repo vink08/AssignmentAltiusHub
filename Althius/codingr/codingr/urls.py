f32057e070ac77b27cb97fbf524cd3c27a9eabc2
Vinay Kumar <110723566+vink08@users.noreply.github.com>
2024-10-29 16:36:55 +0530

    Update urls.py

--- Althius/codingr/codingr/urls.py
+++ Althius/codingr/codingr/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for codingr project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# codingr/urls.py (or the main urls.py file for your Django project)
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth',include('usersystem.urls'))
+    path('api/', include('usersystem.urls')),  # Replace 'your_app_name' with the actual name of your app
 ]
